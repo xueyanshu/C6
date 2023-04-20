4bedc47243a1bdbf2b2623bca23b7612137a2ed5
xueyanshu <82647515+xueyanshu@users.noreply.github.com>
2023-04-20 20:52:56 +0800

    vscode环境配置
    添加一些json文件，安装了runcode、CompileRun插件，安装mingw-w64

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # C6
 《C Primer Plus 6》 homework
+
+1.使用VScode书写代码
+2.做好笔记
---
+++ ch00/test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int num=0;
+    scanf  ("%d",&num);
+    num=num+10;
+    printf("num = %d\n",num);
+
+    return 0;
+}
